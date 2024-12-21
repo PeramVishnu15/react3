258208a85138f0c1c2b98fd90a25d5c9832a3046
PeramVishnu15 <vishnuperam64@gmail.com>
2024-12-21 10:57:28 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Home from './componets/Home'
+import Movies from './componets/Movies'
+
+export default function App() {
+  return (
+    <div>
+
+      <Movies />
+    </div>
+  )
+}
